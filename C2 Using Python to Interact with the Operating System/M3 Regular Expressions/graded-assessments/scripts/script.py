4091351748e94d9a040e2baee6c5ef8bbf63a94e
Indria Agustina <73377271+ind170899@users.noreply.github.com>
2021-03-01 14:43:57 +0700

    Update script.py

--- C2 Using Python to Interact with the Operating System/M3 Regular Expressions/graded-assessments/scripts/script.py
+++ C2 Using Python to Interact with the Operating System/M3 Regular Expressions/graded-assessments/scripts/script.py
@@ -20,8 +20,8 @@ def replace_domain(address, old_domain, new_domain):
 def main():
   """Processes the list of emails, replacing any instances of the old domain with the new domain."""
   old_domain, new_domain = 'abc.edu', 'xyz.edu'
-  csv_file_location = '<csv_file_location>'
-  report_file = '<path_to_home_directory>' + '/updated_user_emails.csv'
+  csv_file_location = '/home/student-01-7dfa47a038e3/data/user_emails.csv'
+  report_file = '/home/student-01-7dfa47a038e3/data' + '/updated_user_emals.csv'
   user_email_list = []
   old_domain_email_list = []
   new_domain_email_list = []
